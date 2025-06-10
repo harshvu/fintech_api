5ab96a5ed152042d68f63e0c355e30e8bc885dd5
infy-sd <sumit.dash@infydigi.com>
2025-06-10 16:11:46 +0530

    files

--- server.js
+++ server.js
@@ -32,6 +32,6 @@ app.set("io", io);
 
 // Start server
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
-  console.log(`🚀 Server running at http://147.93.27.17:${PORT}`);
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`🚀 Server running at http://0.0.0.0:${PORT}`);
 });
