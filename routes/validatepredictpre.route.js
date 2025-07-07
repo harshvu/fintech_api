4e42a5003f74f0863601246be013ec921ce567eb
infy-sd <sumit.dash@infydigi.com>
2025-07-07 18:03:52 +0530

    validate

--- routes/validatepredictpre.route.js
+++ routes/validatepredictpre.route.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const { validatepredictStocks,getSummaryByUser } = require("../controllers/validatepredictController");
+//getSummaryByUser
+const { validatepredictStocks } = require("../controllers/validatepredictController");
 const auth = require("../middlewares/auth.middleware");
 /**
  * @swagger
@@ -23,20 +24,20 @@ const auth = require("../middlewares/auth.middleware");
  */
 router.post("/", validatepredictStocks);
 
-/**
- * @swagger
- * /api/validatepredictpre/latest:
- *   get:
- *     summary: Get latest validation AI prediction per user
- *     tags: [AI validation Prediction For Pre Market Stocks]
- *     responses:
- *       200:
- *         description: Latest predictions fetched
- *       404:
- *         description: No predictions found
- *       500:
- *         description: Internal error
- */
-router.get("/validate_summary", auth, getSummaryByUser);
+// /**
+//  * @swagger
+//  * /api/validatepredictpre/latest:
+//  *   get:
+//  *     summary: Get latest validation AI prediction per user
+//  *     tags: [AI validation Prediction For Pre Market Stocks]
+//  *     responses:
+//  *       200:
+//  *         description: Latest predictions fetched
+//  *       404:
+//  *         description: No predictions found
+//  *       500:
+//  *         description: Internal error
+//  */
+// router.get("/validate_summary", auth, getSummaryByUser);
 
 module.exports = router;
